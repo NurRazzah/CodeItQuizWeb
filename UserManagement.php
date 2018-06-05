9b2d8bf2b1afdad5125c8e1c4fd253826d0c6df8
16022738 <azzah1706@gmail.com>
2018-06-05 18:26:58 +0800

    First commit

---
+++ UserManagement.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="stylesheets/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="sidenav">
+            <h1>ADMINS</h1>
+            <a href="Report.php.php">Report</a>
+            <a href="UserManagement.php.php">User Management</a>
+            <a href="Quiz.php">Quiz</a>
+            <a href="Resources.php">Resources</a>
+        </div>
+    </body>
+</html>
